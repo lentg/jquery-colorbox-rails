9345cb76a3fa189aeb1cb11cbd61152d4fea11eb
lentg <lentg@qq.com>
2013-07-21 19:57:31 +0800

    Update jquery.colorbox.js

--- vendor/assets/javascripts/jquery.colorbox.js
+++ vendor/assets/javascripts/jquery.colorbox.js
@@ -1,5 +1,5 @@
 /*!
-	Colorbox v1.4.26 - 2013-06-30
+	Colorbox v1.4.27 - 2013-07-16
 	jQuery lightbox and modal window plugin
 	(c) 2013 Jack Moore - http://www.jacklmoore.com/colorbox
 	license: http://www.opensource.org/licenses/mit-license.php
@@ -353,14 +353,14 @@
 				// Show colorbox so the sizes can be calculated in older versions of jQuery
 				$box.css({visibility:'hidden', display:'block'});
 				
-				$loaded = $tag(div, 'LoadedContent', 'width:0; height:0; overflow:hidden').appendTo($content);
+				$loaded = $tag(div, 'LoadedContent', 'width:0; height:0; overflow:hidden');
+				$content.css({width:'', height:''}).append($loaded);
 
 				// Cache values needed for size calculations
 				interfaceHeight = $topBorder.height() + $bottomBorder.height() + $content.outerHeight(true) - $content.height();
 				interfaceWidth = $leftBorder.width() + $rightBorder.width() + $content.outerWidth(true) - $content.width();
 				loadedHeight = $loaded.outerHeight(true);
 				loadedWidth = $loaded.outerWidth(true);
-				
 				
 				// Opens inital empty Colorbox prior to content being loaded.
 				settings.w = setSize(settings.initialWidth, 'x');
@@ -404,7 +404,8 @@
 	// Colorbox's markup needs to be added to the DOM prior to being called
 	// so that the browser will go ahead and load the CSS background images.
 	function appendHTML() {
-		if (!$box && document.body) {
+		// if (!$box && document.body) {
+		if (document.body) {
 			init = false;
 			$window = $(window);
 			$box = $tag(div).attr({
@@ -523,7 +524,7 @@
 	}
 
 	// Append the HTML when the DOM loads
-	$(appendHTML);
+	// $(appendHTML);
 
 
 	// ****************
@@ -1056,6 +1057,10 @@
 	// returns a jQuery object.
 	publicMethod.element = function () {
 		return $(element);
+	};
+
+	publicMethod.appendHTML = function() {
+		appendHTML();
 	};
 
 	publicMethod.settings = defaults;
